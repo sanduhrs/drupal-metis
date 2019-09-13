17fb9d2866ef427f71f63b0270a8d41fbda6ad67
Stefan Auditor <stefan.auditor@erdfisch.de>
2019-09-13 14:08:32 +0200

    Issue #3081076: [metis] Metis (VG Wort integration): Update field widget to use new field values

--- src/Plugin/Field/FieldWidget/MetisWidget.php
+++ src/Plugin/Field/FieldWidget/MetisWidget.php
@@ -27,32 +27,27 @@ class MetisWidget extends WidgetBase {
       '#type' => 'textfield',
       '#title' => $this->t('Public code'),
       '#default_value' => isset($items[$delta]->code_public) ? $items[$delta]->code_public : NULL,
-      '#size' => 20,
     ];
 
     $element['code_private'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Private code'),
       '#default_value' => isset($items[$delta]->code_private) ? $items[$delta]->code_private : NULL,
-      '#size' => 20,
+    ];
+
+    $element['show'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show'),
+      '#default_value' => isset($items[$delta]->show) ? $items[$delta]->show : NULL,
     ];
 
     $element['server'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Server'),
       '#default_value' => isset($items[$delta]->server) ? $items[$delta]->server : NULL,
-      '#size' => 20,
-    ];
-
-    $element['used'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('used'),
-      '#default_value' => isset($items[$delta]->used) ? $items[$delta]->used : NULL,
-      '#size' => 20,
     ];
 
     $element['#theme_wrappers'] = ['container', 'form_element'];
-    $element['#attributes']['class'][] = 'container-inline';
     $element['#attributes']['class'][] = 'metis-elements';
     $element['#attached']['library'][] = 'metis/metis';
 
@@ -77,11 +72,11 @@ class MetisWidget extends WidgetBase {
       if ($value['code_private'] === '') {
         $values[$delta]['code_private'] = NULL;
       }
+      if ($value['show'] === '') {
+        $values[$delta]['show'] = NULL;
+      }
       if ($value['server'] === '') {
         $values[$delta]['server'] = NULL;
-      }
-      if ($value['used'] === '') {
-        $values[$delta]['used'] = NULL;
       }
     }
     return $values;
